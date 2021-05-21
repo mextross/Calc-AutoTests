3fe0ae30a4b005c0bf49e7f7c0c1b7b7b09338ee
Michelle Extross <Michelle.Extross@kiwibank.co.nz>
2021-05-21 16:59:34 +1200

    set up page object

---
+++ Calc.TestAutomation/CaclUiTestsCypress/cypress/support/calcPageObjects/calcHomePage.js
@@ -0,0 +1,37 @@
+var calcHomePage = function () {
+
+    var leftNumberInput = '#leftNumber'
+    var rightNumberInput = '#rightNumber'
+    var operatorDropdown = '#operator'
+    var result = 'input.result'
+    var calculatorButton = '#calculate'
+    var iframe = '#root > div > div:nth-child(2) > iframe'
+
+    this.inputLeftNumber = function (leftNum) {
+        cy.get(leftNumberInput).type(leftNum)
+    };
+
+    this.inputRightNumber = function (rightNum) {
+        cy.get(rightNumberInput).type(rightNum)
+    };
+
+    this.selectOperator = function (op) {
+        cy.get(operatorDropdown).select(op);
+    };
+
+    this.clickCalculatorButton = function () {
+        cy.get(iframe).then($iframe => {
+            const $body = $iframe.contents().find('body')
+            cy.wrap($body)
+                .find(calculatorButton)
+                .click()
+        })
+    };
+
+    this.checkResult = function (expectedResult) {
+        cy.get(result).should('have.value', expectedResult)
+    };
+
+    
+};
+module.exports = new calcHomePage();
