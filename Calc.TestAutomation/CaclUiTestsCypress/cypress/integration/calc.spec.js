e421a2b789dc65194e41ec6b29629a326379565a
Michelle Extross <Michelle.Extross@kiwibank.co.nz>
2021-05-21 17:06:18 +1200

    Revert "set up page object"
    This reverts commit 1b9990a5b83cfeac07c2bb4be948b4cca0aff161.

--- Calc.TestAutomation/CaclUiTestsCypress/cypress/integration/calc.spec.js
+++ Calc.TestAutomation/CaclUiTestsCypress/cypress/integration/calc.spec.js
@@ -1,26 +1,42 @@
 import 'cypress-iframe'
-var calcHomePg = require("../support/calcPageObjects/calcHomepage")
 
-Cypress.config('defaultCommandTimeout', 5000);
+//Cypress.config('defaultCommandTimeout', 30000);
 
 context('Calculator', () => {
     beforeEach(() => {
         cy.visit('/')
     })
 
+    
     it('Add 2 positive numbers but clicking and choosing the "+" operator dropdown', () => {
-
-        calcHomePg.inputLeftNumber("1");
-        calcHomePg.inputRightNumber("2");
-        calcHomePg.selectOperator("+");
-        calcHomePg.clickCalculatorButton();
-        calcHomePg.checkResult("3");
+        cy.get('#leftNumber').type("1");
+        cy.get('#rightNumber').type("2");
+        cy.get('#operator').select("+");
+        cy.get('#root > div > div:nth-child(2) > iframe').then($iframe => {
+            const $body = $iframe.contents().find('body')
+            cy.wrap($body)
+                .find('button')
+                .click()
+        })
+        cy.get('input.result').should('have.value', '3')
     })
     
     it('Add 2 positive numbers by leaving the default "+" operator dropdown', () => {
-        calcHomePg.inputLeftNumber("1");
-        calcHomePg.inputRightNumber("2");
-        calcHomePg.clickCalculatorButton();
-        calcHomePg.checkResult("3");
+        cy.get('#leftNumber').type("1");
+        cy.get('#rightNumber').type("2");
+        cy.get('#root > div > div:nth-child(2) > iframe').then($iframe => {
+            const $body = $iframe.contents().find('body')
+            cy.wrap($body)
+                .find('button')
+                .click()
+        })
+        cy.get('input.result').should('have.value', '3')
     })
+
+
+
+
+
+
+
 })
