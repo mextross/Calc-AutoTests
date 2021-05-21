ae2c127eac4accff318f59ae35e51f89d73e1348
Michelle Extross <Michelle.Extross@kiwibank.co.nz>
2021-05-21 14:39:30 +1200

    default dropdown behaviour

--- Calc.TestAutomation/CaclUiTestsCypress/cypress/integration/calc.spec.js
+++ Calc.TestAutomation/CaclUiTestsCypress/cypress/integration/calc.spec.js
@@ -1,25 +1,40 @@
 import 'cypress-iframe'
 
-Cypress.config('defaultCommandTimeout', 30000);
+//Cypress.config('defaultCommandTimeout', 30000);
 
 context('Calculator', () => {
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('Add 2 positive numbers', () => {
-        cy.visit('/')
+    
+    it('Add 2 positive numbers but clicking and choosing the "+" operator dropdown', () => {
         cy.get('#leftNumber').type("1");
         cy.get('#rightNumber').type("2");
-        cy.get('#operator').select("*");
+        cy.get('#operator').select("+");
         cy.get('#root > div > div:nth-child(2) > iframe').then($iframe => {
             const $body = $iframe.contents().find('body')
             cy.wrap($body)
                 .find('button')
                 .click()
         })
-        cy.get('input.result').should('have.value', '2')
+        cy.get('input.result').should('have.value', '3')
     })
+    
+    it('Add 2 positive numbers by leaving the default "+" operator dropdown', () => {
+        cy.get('#leftNumber').type("1");
+        cy.get('#rightNumber').type("2");
+        cy.get('#root > div > div:nth-child(2) > iframe').then($iframe => {
+            const $body = $iframe.contents().find('body')
+            cy.wrap($body)
+                .find('button')
+                .click()
+        })
+        cy.get('input.result').should('have.value', '3')
+    })
+
+
+
 
 
 
