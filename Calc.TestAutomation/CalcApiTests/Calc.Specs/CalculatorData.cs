e3fce363fe15f3d4ec214f77b5d2e6a6ba715533
Michelle Extross <michelle.extross@kiwibank.co.nz>
2021-05-27 17:02:38 +1200

    implemented scenario examples and context injection

---
+++ Calc.TestAutomation/CalcApiTests/Calc.Specs/CalculatorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc.Specs
+{
+    public class CalculatorData
+    {
+        public decimal leftNumber;
+        public decimal rightNumber;
+        public string op;
+        public decimal result;
+    }
+}
